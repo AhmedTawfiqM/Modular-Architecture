e7d8b513ee1adffbc60f50cac27349e86830119b
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2021-07-12 09:26:00 +0200

    adapter TImeLine Architecture Integration DOne

---
+++ common/presentation/src/main/java/com/atdev/presentation/notify/AppToast.kt
@@ -0,0 +1,20 @@
+package com.atdev.presentation.ui.notify
+
+import android.content.Context
+import android.widget.Toast
+
+class AppToast {
+
+
+    fun showLong(context: Context, msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+    }
+
+    fun showShort(context: Context, msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showLong(context: Context, msg: String,duration: Int) {
+        Toast.makeText(context, msg, duration).show()
+    }
+}
